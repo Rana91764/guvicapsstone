4697017bb6b816672a2f575adcf97de590fb22e1
Rana91764 <ranaoct1989@gmail.com>
2025-04-20 17:08:01 +0530

    commit1

--- OneDrive/Desktop/capstone project/Project/Build.sh
+++ OneDrive/Desktop/capstone project/Project/Build.sh
@@ -6,7 +6,7 @@ echo "Current Branch: $BRANCH_NAME"
 if [ "$BRANCH_NAME" == "dev" ]; then
     docker build -t rana1993/dev_guvi . 
     docker push rana1993/dev_guvi:latest
-elif [ "$BRANCH_NAME" == "master" ]; then
+elif [ "$BRANCH_NAME" == "main" ]; then
     docker build -t rana1993/prod_guvi .
     docker push rana1993/prod_guvi:latest
 fi
--- OneDrive/Desktop/capstone project/Project/Deploy.sh
+++ OneDrive/Desktop/capstone project/Project/Deploy.sh
@@ -7,7 +7,7 @@ docker stop myreact-app || true
 docker rm myreact-app || true
 docker pull rana1993/dev_guvi:latest
 docker run -d -p 80:80 --name myreact-app rana1993/dev_guvi:latest
-elif  [ "$BRANCH_NAME" = "master" ]; then
+elif  [ "$BRANCH_NAME" = "main" ]; then
 docker stop myreact-app || true
 docker rm myreact-app || true
 docker pull rana1993/prod_guvi:latest
